5d189efe70acf787fbef2df1c7ec8e6c41bae647
KrishnaMarathe <krishnamarathe19@gmail.com>
2026-07-15 22:22:43 +0530

    Added researcher CRUD APIs and frontend setup

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from app.database import engine, Base
 import app.models
@@ -5,6 +6,13 @@ from app.routes import users, researchers
 import traceback
 
 app = FastAPI(title="Scientific Collaboration Network Analyzer")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
--- app/routes/researchers.py
+++ app/routes/researchers.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app import crud, schemas
+from app import models, schemas, crud
 
 router = APIRouter(
     prefix="/researchers",
